2ca0f048ea9ade134081ddb513f2e181f2353c7d
manishkumar.singh <manish.singh@trainingmug.com>
2023-07-28 20:12:00 +0530

    changed db project

--- api/db/conn.js
+++ api/db/conn.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const URL = "mongodb+srv://ermaanish:manish_singh@cluster0.u4btkwk.mongodb.net/?retryWrites=true&w=majority"
+const URL2 = "mongodb+srv://ermaanish:manish_singh@cluster0.5okciwd.mongodb.net/?retryWrites=true&w=majority" 
+
 const LOCAL_URL="mongodb://127.0.0.1:27017/userdb"
-mongoose.connect(URL,{
+mongoose.connect(URL2,{
    // useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true
