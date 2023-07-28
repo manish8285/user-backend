67c8131d2b9801815a400b3b71e93f22d088c4a1
manishkumar.singh <manish.singh@trainingmug.com>
2023-07-29 00:36:08 +0530

    added env

--- api/db/conn.js
+++ api/db/conn.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
-const URL = "mongodb+srv://ermaanish:manish_singh@cluster0.u4btkwk.mongodb.net/?retryWrites=true&w=majority"
-const URL2 = "mongodb+srv://ermaanish:manish_singh@cluster0.5okciwd.mongodb.net/?retryWrites=true&w=majority" 
+require("dotenv").config()
+require("dotenv").config({ path: `.env.local`, override: true })
+// const URL = "mongodb+srv://ermaanish:manish_singh@cluster0.u4btkwk.mongodb.net/?retryWrites=true&w=majority"
+// const URL2 = "mongodb+srv://ermaanish:manish_singh@cluster0.5okciwd.mongodb.net/?retryWrites=true&w=majority" 
 
 const LOCAL_URL="mongodb://127.0.0.1:27017/userdb"
-mongoose.connect(URL2,{
+mongoose.connect(process.env.MONGODB_URI,{
    // useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true
